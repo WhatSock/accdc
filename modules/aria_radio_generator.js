6d1b0de276a6926f1c9bc5cb24bf2195dfe7091f
Bryan Garaventa <bryan.garaventa@whatsock.com>
2013-02-16 15:14:09 -0800

    Updated the ARIA Radio module to support deep searches for Radios in prep for the Technical Style Guide release

--- modules/aria_radio_generator.js
+++ modules/aria_radio_generator.js
@@ -1,28 +1,28 @@
 /*!
-ARIA Radio Generator R1.0
-Instructions: http://lnkd.in/EWzQch
+ARIA Radio Generator R2.0
 Copyright 2010-2013 Bryan Garaventa (WhatSock.com)
 Part of AccDC, a Cross-Browser JavaScript accessibility API, distributed under the terms of the Open Source Initiative OSI - MIT License
 	*/
 
 (function(){
 
-// Send an ID or DOM node as the radioGroupObj parameter, set the default selection index value, specify the legend text, and define a callback function
-	$A.RadioGroup = function(radioGroupObj, defaultRadioVal, legendText, callback){
-		if (!legendText)
-			alert('legentText is required as a shared group label.');
-
+// Send an ID or DOM node as the radioGroupObj parameter, set the CSS Selector for all child radio elements, set the default selection index value, specify the legend text, and define a callback function
+// Set defaultRadioVal to -1 to prevent a default selection from being made automatically
+	$A.RadioGroup = function(radioGroupObj, selector, defaultRadioVal, legendText, callback){
 		var rgo = typeof radioGroupObj === 'string' ? document.getElementById(radioGroupObj) : radioGroupObj, track = {},
 			that = this, select = function(index, force){
-			for (var i = 0; i < that.childNodes.length - 1; i++)
-							$A.setAttr(that.childNodes[i],
+			if (isNaN(index))
+				index = 0;
+
+			for (var i = 0; i < that.radios.length; i++)
+							$A.setAttr(that.radios[i],
 											{
 											tabindex: '-1',
 											'aria-selected': 'false',
 											'aria-checked': 'false'
 											});
 
-			$A.setAttr(that.childNodes[index],
+			$A.setAttr(that.radios[index],
 							{
 							tabindex: '0',
 							'aria-selected': 'true',
@@ -30,59 +30,92 @@ Part of AccDC, a Cross-Browser JavaScript accessibility API, distributed under t
 							});
 
 			if (force)
-				that.childNodes[index].focus();
-			that.selected = that.childNodes[index];
+				that.radios[index].focus();
+			that.value = that.radios[index].id;
 			that.index = index;
 
 			if (callback && typeof callback === 'function')
-				callback(that.childNodes[index], that.childNodes);
+				callback.apply(that,
+								[
+								that.radios[index],
+								that.radios
+								]);
 		};
+
 		$A.setAttr(rgo, 'aria-label', legendText);
-		that.childNodes = $A.query('#' + rgo.id + ' > *', rgo.parentNode, function(i, o){
+		var ids = [];
+		that.radios = $A.query(selector, rgo, function(i, o){
 			track[o.id] = i;
 			track.max = i + 1;
 			$A.setAttr(o,
 							{
-							tabindex: '-1',
+							tabindex: (i === 0 && defaultRadioVal === -1) ? '0' : '-1',
 							'aria-selected': 'false',
 							'aria-checked': 'false',
 							'aria-posinset': track.max
 							});
 
-			if (!(o.innerText || o.textContent) && !getAttr(o, 'title') && !getAttr(o, 'aria-label'))
-				alert('The radio button tag with id=' + o.id
-					+ ' is lacking inner text. Either offscreen text or visible text must be added between the opening and closing tags.');
+			ids.push(o.id);
 		});
+		$A.setAttr(rgo, 'aria-owns', ids.join(' '));
 
-		for (var i = 0; i < that.childNodes.length - 1; i++)
-						$A.setAttr(that.childNodes[i], 'aria-setsize', track.max);
+		for (var i = 0; i < that.radios.length; i++)
+						$A.setAttr(that.radios[i], 'aria-setsize', track.max);
 
-		$A.bind(that.childNodes, 'click', function(ev){
-			select(track[this.id]);
-		});
-		$A.bind(that.childNodes, 'keydown', function(ev){
-			var k = ev.which || ev.keyCode;
+		$A.bind(that.radios,
+						{
+						click: function(ev){
+							select(track[this.id]);
+							ev.preventDefault();
+						},
+						keydown: function(ev){
+							var k = ev.which || ev.keyCode;
 
-			if (k == 37 || k == 38){
-				if (that.index > 0)
-					select(that.index - 1, true);
+							if (k == 37 || k == 38){
+								ev.preventDefault();
 
-				else
-					select(track.max - 1, true);
-			}
+								if (isNaN(that.index))
+									that.index = 0;
 
-			else if (k == 39 || k == 40){
-				if (that.index < (track.max - 1))
-					select(that.index + 1, true);
+								if (that.index > 0)
+									select(that.index - 1, true);
 
-				else
-					select(0, true);
-			}
-		});
+								else
+									select(track.max - 1, true);
+							}
+
+							else if (k == 39 || k == 40){
+								ev.preventDefault();
+
+								if (isNaN(that.index))
+									that.index = 0;
+
+								if (that.index < (track.max - 1))
+									select(that.index + 1, true);
+
+								else
+									select(0, true);
+							}
+						},
+						keypress: function(ev){
+							var k = ev.which || ev.keyCode;
+
+							if (k == 13 || k == 32){
+								ev.preventDefault();
+								$A.trigger(this, 'click');
+							}
+						}
+						});
 
 		that.set = function(index){
-			select(index);
+			if (( typeof index === 'string' && !index) || ( typeof index === 'number' && index < 0)
+				|| ( typeof index === 'object'))
+				return;
+
+			select( typeof index === 'string' ? track[index] : index);
 		};
-		select( typeof defaultRadioVal === 'number' ? defaultRadioVal : 0);
+
+		if (defaultRadioVal !== -1)
+			select(isNaN(defaultRadioVal) ? 0 : defaultRadioVal);
 	};
 })();

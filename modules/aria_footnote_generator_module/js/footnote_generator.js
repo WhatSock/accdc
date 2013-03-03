0922c771ee077cb5b306cbc8e82e7564493ecd74
Bryan Garaventa <bryan.garaventa@whatsock.com>
2013-03-02 22:53:20 -0800

    Updated the Footnotes module to support auto configuration using HTML5 in prep for the Technical Style Guide release

--- modules/aria_footnote_generator_module/js/footnote_generator.js
+++ modules/aria_footnote_generator_module/js/footnote_generator.js
@@ -1,107 +1,65 @@
 /*!
-ARIA Footnote Generator Module R1.0
+ARIA Footnote Generator Module R2.0
 Copyright 2010-2013 Bryan Garaventa (WhatSock.com)
 Part of AccDC, a Cross-Browser JavaScript accessibility API, distributed under the terms of the Open Source Initiative OSI - MIT License
 */
 
 (function(){
 
-	$A.setFootnotes = function(path, mode, config){
+	$A.setFootnotes = function(selector, context, config){
 
-		var config = config || {}, clsName = config.className || 'footnote', fnText = config.fnText || '_Footnote',
-			targId = config.targId || 'fns', hMax = config.hMax || 1;
+		var config = config || {}, fnChar = config.fnChar || '&#8224;', fnText = config.fnText || 'Footnote',
+			backText = config.backText || 'Back to Footnote', pair = {}, context = context || document;
 
-		$A.setFootnotes.generateFootnotes = function(footnotes){
+		$A.query(selector, context, function(i, o){
+			var fnId = $A.getAttr(o, 'data-footnote'), index = i;
 
-			var targ = $A.getEl(targId), inc = 0, t = $A.createEl('table'), d = null;
-
-			$A.query('span.footnote', function(i, f){
-				$A.setAttr(f,
+			if (!pair[fnId])
+				pair[fnId] =
 								{
-								role: 'link',
-								tabindex: '0'
-								});
+								fns: [],
+								last: 0,
+								name: $A.getText(o),
+								targ: $A.getEl(fnId)
+								};
 
-				f.appendChild($A.createEl('span', null, null, 'screenReading', document.createTextNode(fnText)));
-				f.appendChild($A.createEl('sup', null, null, null, document.createTextNode(i + 1)));
-				var td = $A.createEl('td'), p = $A.createEl('div'), n = $A.createEl('div',
-								{
-								role: 'link',
-								tabindex: '0'
-								}, null, 'footnote'), m = $A.createEl('div');
+			var a = $A.createEl('a',
+							{
+							href: '#',
+							title: fnText + (i + 1),
+							'aria-label': fnText + (i + 1)
+							}, null, null, $A.createEl('sup',
+							{
+							'aria-hidden': 'true'
+							}, null, null, document.createTextNode(i + 1)));
 
-				n.innerHTML = '<span class="fnIndex">&#8224; [<span class="screenReading">' + fnText + '</span>' + (i + 1)
-					+ '] </span>' + footnotes['-' + f.id];
-				p.appendChild(n);
-				p.appendChild(m);
-				$A.query('a', n, function(){
-					m.appendChild(this);
-				});
-				td.appendChild(p);
+			pair[fnId].fns.push(a);
+			o.appendChild(a);
 
-				if (inc == 0 || hMax === 1){
-					d = $A.createEl('tr');
-					t.appendChild(d);
-					inc = 1;
-				}
-
-				else if (inc >= (hMax - 1))
-					inc = 0;
-
-				else
-					inc += 1;
-				d.appendChild(td);
-				$A.bind(f,
-								{
-								keypress: function(ev){
-									var k = ev.which || ev.keyCode;
-
-									if (k == 13 || k == 32){
-										n.focus();
-										ev.preventDefault();
-									}
-								},
-								click: function(ev){
-									n.focus();
-									ev.preventDefault();
-								}
-								});
-				$A.bind(n,
-								{
-								keypress: function(ev){
-									var k = ev.which || ev.keyCode;
-
-									if (k == 13 || k == 32){
-										f.focus();
-										ev.preventDefault();
-									}
-								},
-								click: function(ev){
-									f.focus();
-									ev.preventDefault();
-								}
-								});
+			$A.bind(a, 'click', function(ev){
+				pair[fnId].last = $A.inArray(this, pair[fnId].fns);
+				pair[fnId].fn.focus();
+				ev.preventDefault();
 			});
 
-			targ.appendChild(t);
-		};
+			if (!pair[fnId].fn){
+				var a2 = $A.createEl('a',
+								{
+								href: '#',
+								title: backText + ' ' + pair[fnId].name,
+								'aria-label': backText + ' ' + pair[fnId].name
+								}, null, null, $A.createEl('span',
+								{
+								'aria-hidden': 'true'
+								}, null, null, document.createTextNode(fnChar)));
 
-		if (!mode)
-			$A.getScript(path);
-
-		else if (mode === 1){
-			var fns = {}, tmp = $A.createEl('div'), q = $A.query('span.footnote'), inc1 = 0, inc2 = q.length;
-
-			for (var i = 0; i < inc2; i++){
-				var f = q[i];
-				$A.load(tmp, path + ' #-' + f.id, function(v){
-					fns['-' + f.id] = tmp.innerHTML;
-					inc1 += 1;
-
-					if (inc1 === inc2)
-						$A.setFootnotes.generateFootnotes(fns);
+				pair[fnId].fn = a2;
+				pair[fnId].targ.appendChild(a2);
+				$A.bind(a2, 'click', function(ev){
+					pair[fnId].fns[pair[fnId].last].focus();
+					ev.preventDefault();
 				});
 			}
-		}
+		});
 	};
 })();
--- modules/aria_footnote_generator_module/js/setup.js
+++ modules/aria_footnote_generator_module/js/setup.js
@@ -1,23 +1,19 @@
-
 $A.bind(window, 'load', function(){
 
-	// Using an external object literal with JS
-	$A.setFootnotes('files/footnotes.js', 0,
+// Configure the footnotes within the document
+
+// "accFootnote" is the class name selector that points to the Span tags within the document that are to become footnotes
+
+	$A.setFootnotes('.accFootnote', document,
 					{
-					className: 'footnote',
-					fnText: '_Footnote',
-					targId: 'fns',
-					hMax: 1
+
+					// Set the tooltip text for the footnote (this will also be the accessible name for screen reader users)
+					fnText: 'Footnote',
+
+					// Set the footnote character or text that will comprise the visual link text for returning footnotes
+					fnChar: '&#8224;',
+
+					// Set the tooltip text for the footnote back links (this will also be the accessible name for screen reader users)
+					backText: 'Back to Footnote'
 					});
-
-/*
-// Or using an external HTML markup file
-$A.setFootnotes('files/footnotes.html', 1, {
-className: 'footnote',
-fnText: '_Footnote',
-targId: 'fns',
-hMax: 1
-});
-*/
-
 });
